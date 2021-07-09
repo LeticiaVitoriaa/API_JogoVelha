40de77e48db88dc3cb8bb287bef1afdc699d5fa2
Letícia Vitória <leticiav_gomes@hotmail.com>
2021-07-09 17:19:18 -0300

    game

---
+++ game.js
@@ -0,0 +1,12 @@
+function NovoPartida() {
+    let npartida = document.getElementById();
+    return npartida;
+
+}
+
+function SorteiaJogador() {
+    const firstPlayer;
+    firstPlayer = Math.random("X", "O");
+    
+    return firstPlayer;
+}
